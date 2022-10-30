ab6f015b084178685e594f010d59ed8e81dd859b
hiroto-okumura <hiroto_okumura@yahoo.co.jp>
2022-10-30 17:40:13 +0900

    testing mock of Drag and Drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import './App.css';
 
 function App() {
+  // items[]の配列をDBと連携できれば、DBの内容を画面に出していじれそう
+  const [items] = useState([
+    { id: 0, text: "item0" },
+    { id: 1, text: "item1" },
+    { id: 2, text: "item2" }
+  ]);
+
+  const onDragEnd = (result) => {
+    const remove = items.splice(result.source.index, 1)
+    console.log(remove);
+    items.splice(result.destination.index, 0, remove[0]);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dragDropArea">
+      {/* 要素をつかんで離したときに、発火するonDragEnd関数 */}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="dp_1">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {items.map((item, index) => (
+
+                <Draggable draggableId={item.text} index={item.id} key={item.id}>
+                  {(provided) => (
+                    <div className="item"
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      {item.text}
+                    </div>)}
+                </Draggable>
+              ))}
+
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
